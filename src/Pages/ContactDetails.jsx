90083aa18288319ce6a7811d591b6a6aa8c184a5
habiba2m <habibamohamadeen@gmail.com>
2024-09-14 15:18:38 +0300

    refactor: add contact info in a separate file

--- src/Pages/ContactDetails.jsx
+++ src/Pages/ContactDetails.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import ContactInfo from "../Components/ContactInfo";
 
 function ContactDetails() {
   const location = useLocation();
@@ -6,25 +7,6 @@ function ContactDetails() {
 
   return (
     <div>{contact ? <ContactInfo contact={contact} /> : <p>Loading...</p>}</div>
-  );
-}
-
-function ContactInfo({ contact }) {
-  return (
-    <div className="contact-container">
-      <img src={contact.picture.large} alt="profile" />
-      <div>
-        <p className="contact-name">
-          {contact.name.first} {contact.name.last}
-        </p>
-        <p>📩 {contact.email}</p>
-        <p>☎️ {contact.phone}</p>
-        <p>
-          📍{contact.location.city}, {contact.location.state},{" "}
-          {contact.location.country}
-        </p>
-      </div>
-    </div>
   );
 }
 
